75c202b1020e5ebee97f16c782247b986239fa39
arashi1214 <opp40519@gmail.com>
2023-04-17 16:48:00 +0800

    更改訓練集

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # 日文訓練集
+* https://github.com/tesseract-ocr/tessdata_best
 * https://github.com/zodiac3539/jpn_vert
+
 # pytesseract.image_to_string config 參數
 * --psm 0自動判斷
 * --psm 3 橫向
--- test.py
+++ test.py
@@ -16,14 +16,14 @@ def pdf_to_png(path):
 
 # ===== 圖像辨識 =====#
 def image_to_txt(path_list, file_name):
-  pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+  pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
   file = open(file_name, "a", encoding="utf-8")
 
   for page in path_list:
       img = Image.open(page)
-      text = pytesseract.image_to_string(img, lang='jpn', config='--psm 3 -c preserve_interword_spaces=1')
+      text = pytesseract.image_to_string(img, lang='jpn_vert', config='--psm 3 -c preserve_interword_spaces=1')
       file.write(text)
 
 # page_list = pdf_to_png('messageImage_1679045745113.pdf')
 page_list = ['messageImage_1679045745113.jpg']
-image_to_txt(page_list, "output.txt")
+image_to_txt(page_list, "output2.txt")

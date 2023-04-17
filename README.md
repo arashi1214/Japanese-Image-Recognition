97ab2acf90e68d41b73ac7b6d340a9749dc5eff7
arashi1214 <opp40519@gmail.com>
2023-04-17 16:23:00 +0800

    日文辨識初步建立

---
+++ README.md
@@ -0,0 +1,6 @@
+# 日文訓練集
+* https://github.com/zodiac3539/jpn_vert
+# pytesseract.image_to_string config 參數
+* --psm 0自動判斷
+* --psm 3 橫向
+* --psm 6 直向
---
+++ test.py
@@ -0,0 +1,29 @@
+import pytesseract
+from PIL import Image
+# import module
+from pdf2image import convert_from_path
+ 
+ # ===== 轉 pdf to png =====#
+# Store Pdf with convert_from_path function
+def pdf_to_png(path):
+  images = convert_from_path(path, poppler_path='C:\\Program Files\\poppler-0.68.0\\bin')
+  page_list = []
+  for i in range(len(images)):
+        # Save pages as images in the pdf
+      images[i].save('page'+ str(i) +'.png', 'PNG')
+      page_list.append('page'+ str(i) +'.png')
+  return page_list
+
+# ===== 圖像辨識 =====#
+def image_to_txt(path_list, file_name):
+  pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+  file = open(file_name, "a", encoding="utf-8")
+
+  for page in path_list:
+      img = Image.open(page)
+      text = pytesseract.image_to_string(img, lang='jpn', config='--psm 3 -c preserve_interword_spaces=1')
+      file.write(text)
+
+# page_list = pdf_to_png('messageImage_1679045745113.pdf')
+page_list = ['messageImage_1679045745113.jpg']
+image_to_txt(page_list, "output.txt")
